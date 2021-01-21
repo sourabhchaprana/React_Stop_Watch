01a85bd8924755bbdbd3c724706553da9cb9c450
sourabh <sourabhchaprana@gmail.com>
2021-01-21 14:02:43 +0530

    Code Successfully Uploaded on Github

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimerOn] = React.useState(false);
+  const [count, setcount] = React.useState(0);
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!timerOn) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
+  const handleIncreament = () => {
+    if (count <= 9) {
+      setcount(count + 1);
+    }
+  };
+  const handleDecreament = () => {
+    if (count >= -9) {
+      setcount(count - 1);
+    }
+    
+  };
+
+  return (
+    <div className="Timers">
+      <h2>Stopwatch</h2>
+      <div id="display">
+        <span>{("0" + Math.floor(time / 60000)).slice(-2)}:</span>
+        <span>{("0" + Math.floor(time / 1000)).slice(-2)}:</span>
+        <span>{("0" + time / 10).slice(-2)}</span>
+      </div>
+
+      <div id="buttons">
+        {!timerOn && time === 0 && (
+          <button onClick={() => setTimerOn(true)}>Start</button>
+        )}
+        {timerOn && <button onClick={() => setTimerOn(false)}>Stop</button>}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTime(0)}>Reset</button>
+        )}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTimerOn(true)}>Resume</button>
+        )}
+      </div>
+      <br />
+      <br />
+      <button onClick={handleIncreament}>+</button>
+      {count}
+      <button onClick={handleDecreament}>-</button>
+    </div>
+  );
+};
+
+export default App;
